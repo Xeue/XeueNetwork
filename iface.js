b625172b9b516cf1217c229f040ce02d5ee9e9c7
Sam Chilton <sam@xeue.uk>
2023-11-29 21:32:26 +0000

    Added bridge and setIP

--- iface.js
+++ iface.js
@@ -13,57 +13,81 @@ export default class Iface {
   /*
   * [
   *   {
-  *     interface: 'eth0',
-  *     link: 'ethernet',
-  *     address: 'b8:27:eb:da:52:ad',
-  *     ipv4_address: '192.168.1.2',
-  *     ipv4_broadcast: '192.168.1.255',
-  *     ipv4_subnet_mask: '255.255.255.0',
-  *     up: true,
-  *     broadcast: true,
-  *     running: true,
-  *     multicast: true
-  *   },
-  *   {
-  *     interface: 'lo',
-  *     link: 'local',
-  *     ipv4_address: '127.0.0.1',
-  *     ipv4_subnet_mask: '255.0.0.0',
-  *     up: true,
-  *     running: true,
-  *     loopback: true
-  *   },
-  *   {
-  *     interface: 'wlan0',
-  *     link: 'ethernet',
-  *     address: '00:0b:81:95:12:21',
-  *     ipv4_address: '192.168.10.1',
-  *     ipv4_broadcast: '192.168.10.255',
-  *     ipv4_subnet_mask: '255.255.255.0',
-  *     up: true,
-  *     broadcast: true,
-  *     multicast: true
+  *       "ifindex": 1,
+  *       "ifname": "lo",
+  *       "flags": [
+  *           "LOOPBACK",
+  *           "UP",
+  *           "LOWER_UP"
+  *       ],
+  *       "mtu": 65536,
+  *       "qdisc": "noqueue",
+  *       "operstate": "UNKNOWN",
+  *       "group": "default",
+  *       "txqlen": 1000,
+  *       "link_type": "loopback",
+  *       "address": "00:00:00:00:00:00",
+  *       "broadcast": "00:00:00:00:00:00",
+  *       "addr_info": [
+  *           {
+  *               "family": "inet",
+  *               "local": "127.0.0.1",
+  *               "prefixlen": 8,
+  *               "scope": "host",
+  *               "label": "lo",
+  *               "valid_life_time": 4294967295,
+  *               "preferred_life_time": 4294967295
+  *           },
+  *           {
+  *               "family": "inet6",
+  *               "local": "::1",
+  *               "prefixlen": 128,
+  *               "scope": "host",
+  *               "valid_life_time": 4294967295,
+  *               "preferred_life_time": 4294967295
+  *           }
+  *       ]
   *   }
   * ]
   */
   async status(iface = '') {
-    const command =  iface == '' ? 'ifconfig -a' : `ifconfig ${iface}`;
+    const command =  iface == '' ? 'ip -j a' : `ip -j a s ${iface}`;
     const {stdout} = await this.Shell.run(this.sudo+command, false);
-    if (iface == '') return this.#parse_status(stdout[0]);
-    else return [this.#parse_status_block(stdout[0].trim())];
+    const ifaces = JSON.parse(stdout.join(''));
+    const routesOut = await this.Shell.run(this.sudo+'ip -j r list', false);
+    const routes = JSON.parse(routesOut.stdout.join('')).filter(route=>route.dst=='default');
+    const routesDev = routes.map(route=>route.dev);
+    ifaces.forEach(iface => {
+      if (routesDev.includes(iface.ifname)) {
+        iface.gateway = routes[routesDev.indexOf(iface.ifname)].gateway;
+      }
+    })
+    return ifaces;
+  }
+
+  async setIP(iface, addrObj) {
+    if (!addrObj.mask) return
+    if (addrObj.ipv4) await this.Shell.run(this.sudo+`ip addr add ${addrObj.ipv4}/${addrObj.mask} dev ${iface}`, false);
+    if (addrObj.gateway) await this.Shell.run(this.sudo+`ip route add default ${addrObj.gateway}/${addrObj.mask} dev ${iface}`, false);
+  }
+
+  async bridgeAdd(iface) {
+    await this.Shell.run(`ip link add name br0 type bridge`);
+    await this.Shell.run(`ip link set dev br0 up`);
+    await this.Shell.run(`ip link set dev ${iface} master br0`);
+  }
+
+  async bridgeRemove(iface) {
+    await this.Shell.run(`ip link set dev ${iface} nomaster`);
   }
 
   async down(iface) {
-    const {stdout} = await this.Shell.run(this.sudo+`ifconfig ${iface} down`, false);
+    const {stdout} = await this.Shell.run(this.sudo+`ip link set dev ${iface} down`, false);
     return stdout[0];
   }
 
-  async up(iface, options = {}) {
-    let optionsText = '';
-    options.forEach((option, value) => {
-      optionsText += option == 'address' ? value+' ' : `${option} ${value} `;
-    });
-    const {stdout} = await this.Shell.run(this.sudo+`ifconfig ${iface}${optionsText} up`, false);
+  async up(iface) {
+    const {stdout} = await this.Shell.run(this.sudo+`ip link set dev ${iface} up`, false);
     return stdout[0];
   }
 
