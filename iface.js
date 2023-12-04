4857923f5fbb31796d2bf9ce924428dd0c9f1883
Sam Chilton <sam@xeue.uk>
2023-12-04 21:50:41 +0000

    Added watcher

--- iface.js
+++ iface.js
@@ -1,13 +1,17 @@
 import {Shell as _Shell} from 'xeue-shell';
+import EventEmitter from 'events';
 Object.prototype.forEach = function(callback) {for (const key in this) {
 	if (Object.hasOwnProperty.call(this, key)) callback.call(this, key, this[key]);
 }}
 
-export default class Iface {
+export default class Iface extends EventEmitter {
 	constructor(Logs, sudo = false) {
+    super();
     this.Logs = Logs;
     this.Shell = new _Shell(this.Logs, 'NETWRK', 'W', 'bash');
     this.sudo = sudo ? 'sudo ' : '';
+    this.watcher = this.Shell.process('ip -s monitor label', false);
+    this.watcher.on('stdout', message => this.#doWatch(message))
   }
 
   /*
@@ -96,81 +100,15 @@ export default class Iface {
     return stdout[0];
   }
 
-
   async stopDHCP(iface) {
     const {stdout} = this.Shell.run(this.sudo+'kill `pgrep -f "^udhcpc -i ' + iface + '"` || true', false);  
     return stdout[0];
   }
-  
-  #parse_status_block(block) {
-    let match;
 
-    const parsed = {
-      interface: block.match(/^([^\s^\:]+)/)[1]
-    };
-
-    if ((match = block.match(/Link encap:\s*([^\s]+)/))) {
-      parsed.link = match[1].toLowerCase();
-    }
-
-    if ((match = block.match(/HWaddr\s+([^\s]+)/))) {
-      parsed.address = match[1].toLowerCase();
-    } else if ((match = block.match(/ether\s+([^\s]+)/))) {
-      parsed.address = match[1].toLowerCase();
-    }
-
-    if ((match = block.match(/inet6\s+addr:\s*([^\s]+)/))) {
-      parsed.ipv6_address = match[1];
-    } else if ((match = block.match(/inet6\s*([^\s]+)/))) {
-      parsed.ipv6_address = match[1];
-    }
-
-    if ((match = block.match(/inet\s+addr:\s*([^\s]+)/))) {
-      parsed.ipv4_address = match[1];
-    } else if ((match = block.match(/inet\s+([^\s]+)/))) {
-      parsed.ipv4_address = match[1];
-    }
-
-    if ((match = block.match(/Bcast:\s*([^\s]+)/))) {
-      parsed.ipv4_broadcast = match[1];
-    } else if ((match = block.match(/broadcast\s*([^\s]+)/))) {
-      parsed.ipv4_broadcast = match[1];
-    }
-
-    if ((match = block.match(/Mask:\s*([^\s]+)/))) {
-      parsed.ipv4_subnet_mask = match[1];
-    } else if ((match = block.match(/netmask\s*([^\s]+)/))) {
-      parsed.ipv4_subnet_mask = match[1];
-    }
-
-    if ((match = block.match(/UP/))) {
-      parsed.up = true;
-    }
-
-    if ((match = block.match(/BROADCAST/))) {
-      parsed.broadcast = true;
-    }
-
-    if ((match = block.match(/RUNNING/))) {
-      parsed.running = true;
-    }
-
-    if ((match = block.match(/MULTICAST/))) {
-      parsed.multicast = true;
-    }
-
-    if ((match = block.match(/LOOPBACK/))) {
-      parsed.loopback = true;
-    }
-
-    return parsed;
-  }
-
-  #parse_status(stdout) {
-    const ifaces = stdout.trim().split('\n\n').map(this.#parse_status_block);
-    ifaces.forEach(iface => {
-      if (iface.interface == 'lo') ifaces.splice(ifaces.indexOf(iface), 1);
-    });
-    return ifaces;
+  #doWatch(message) {
+    const messageArr = message.split('\n');
+    messageArr.forEach(msg => {
+      if (msg.includes('[ADDR]')) this.emit('addr');
+    })
   }
 }
